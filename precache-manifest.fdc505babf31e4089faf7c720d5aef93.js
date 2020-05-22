4fbc38d256d9c706dc39b0952849f9ee16961711
Irina <irina.potrebko7@gmail.com>
2020-05-22 11:00:34 +0300

    Updates

---
+++ precache-manifest.fdc505babf31e4089faf7c720d5aef93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e247e2412919b6394fa44df49494fa67",
+    "url": "/dog-game/index.html"
+  },
+  {
+    "revision": "4c6e166781d90fd029c4",
+    "url": "/dog-game/static/css/main.63f0129e.chunk.css"
+  },
+  {
+    "revision": "e7918ac36c26af9b1885",
+    "url": "/dog-game/static/js/2.a5719e73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dog-game/static/js/2.a5719e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6e166781d90fd029c4",
+    "url": "/dog-game/static/js/main.3bf1ec8c.chunk.js"
+  },
+  {
+    "revision": "836fba04d66096230ba0",
+    "url": "/dog-game/static/js/runtime-main.05317e42.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/dog-game/static/media/dog.75fa68d4.mp3"
+  }
+]);
