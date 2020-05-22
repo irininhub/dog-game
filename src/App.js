fe825d344373c7b3232683116e4a41186532ad0b
Irina <irina.potrebko7@gmail.com>
2020-05-22 17:31:55 +0300

    add media queries

--- src/App.js
+++ src/App.js
@@ -5,70 +5,71 @@ import dog from './dog.mp3'
 
 class App extends React.Component {
 
-    componentDidMount() {
-        this.getRandomImageIndex()
+  componentDidMount() {
+    this.getRandomImageIndex()
+  }
+
+  state = {
+    randomId: 0,
+    cell: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16],
+    counter: 0,
+  };
+
+  audio = new Audio(dog);
+
+  onAddCounter = (clickId) => {
+    if (clickId === this.state.randomId) {
+      this.audio.currentTime = 0;
+      this.audio.play();
+      this.setState({
+        counter: this.state.counter + 1,
+      });
     }
-
-    state = {
-        randomId: 0,
-        cell: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16],
-        counter: 0,
-    };
-
-    audio = new Audio(dog);
-
-    onAddCounter = (clickId) => {
-        if (clickId === this.state.randomId) {
-            this.audio.currentTime = 0;
-            this.audio.play();
-            this.setState({
-                counter: this.state.counter+1,
-            });
-        }
-    };
+  };
 
 
-    getRandomImageIndex = () => {
-        this.setInterval();
-        let randomImgIndex = Math.floor(Math.random() * 9);
-        this.setState({
-            randomId: randomImgIndex
-        })
-    };
+  getRandomImageIndex = () => {
+    this.setInterval();
+    let randomImgIndex = Math.floor(Math.random() * 9);
+    this.setState({
+      randomId: randomImgIndex
+    })
+  };
 
-    setInterval = () => {
-        setTimeout(this.getRandomImageIndex, 800)
-    };
-
-
-    resetGame = () => {
-        this.setState({counter: 0})
-    };
+  setInterval = () => {
+    setTimeout(this.getRandomImageIndex, 800)
+  };
 
 
-    render = () => {
-        let dogsImg = this.state.cell.map((el, ind) => <MaybeDog id={ind}
-                                                                 randomId={this.state.randomId}
-                                                                 onAddCounter={this.onAddCounter}
+  resetGame = () => {
+    this.setState({counter: 0})
+  };
 
-        />)
 
-        return (
-            <div className='app'>
-                <div>
-                    <button className='button'
-                            onClick={this.resetGame}
-                    >RESTART</button>
-                </div>
+  render = () => {
+    let dogsImg = this.state.cell.map((el, ind) => <MaybeDog id={ind}
+                                                             randomId={this.state.randomId}
+                                                             onAddCounter={this.onAddCounter}
 
-                <div className='wrapper'>
-                    <h1 className='name'>ANGRY DOG</h1>
-                    <div className='dogsField'> {dogsImg} </div>
-                    <div className='counter'>{this.state.counter}</div>
-                </div>
-            </div>
-        );
-    }
+    />)
+
+    return (
+      <div className='app'>
+        <div>
+          <button className='button'
+                  onClick={this.resetGame}
+          >RESTART
+          </button>
+        </div>
+
+        <div className='wrapper'>
+          <h1 className='name'>ANGRY DOG</h1>
+          <div className='dogsField'> {dogsImg} </div>
+          <div className='counter'>{this.state.counter}</div>
+        </div>
+      </div>
+    );
+  }
 
 }
 
